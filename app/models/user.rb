f1d5a47bbf41a6efd59e7b5244fc8c43029f3eab
Joshua Calderon <jdcalderon949@gmail.com>
2016-05-03 18:49:03 -0700

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,15 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	def send_activation_email
+		User.Mailer.account_activation(self).deliver
+	end
+	
 	private
 
 	def downcase_email
@@ -46,4 +55,5 @@ class User < ActiveRecord::Base
 		self.activation_token = User.new_token
 		self.activation_digest = User.digest(activation_token)
 	end
+
 end
